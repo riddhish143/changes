8913ba5c49e86178afefce49487df319125d1864
Riddhish Ganesh Mahajan <103731074+riddhish143@users.noreply.github.com>
2025-03-23 12:33:05 +0530

    Update apiService.js

--- frontend/src/utils/apiService.js
+++ frontend/src/utils/apiService.js
@@ -4,7 +4,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://changes-lwdc.onrender.com/api';
 
 /**
  * Fetch content from GitHub repository
